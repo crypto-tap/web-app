a2c221aac09e9266dd4c8a9f4d1de97a2157ebe9
Willian R. Sabião <willianjedai@gmail.com>
2022-09-11 08:51:21 -0300

    test: remove ssr from components

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -7,21 +7,40 @@ import {
   Input,
   FormHelperText,
 } from '@chakra-ui/react'
-import Invest from '../../ui/components/Invest'
-import Mint from '../../ui/components/Mint'
-import SafeKeeping from '../../ui/components/SafeKeeping'
-import Withdrawal from '../../ui/components/Withdrawal'
+// import Invest from '../../ui/components/Invest'
+// import Mint from '../../ui/components/Mint'
+// import SafeKeeping from '../../ui/components/SafeKeeping'
+// import Withdrawal from '../../ui/components/Withdrawal'
+import dynamic from 'next/dynamic'
 
+const DynamicMint = dynamic(() => import('../../ui/components/Mint'), {
+  ssr: false,
+})
+const DynamicInvest = dynamic(() => import('../../ui/components/Invest'), {
+  ssr: false,
+})
+const DynamicSafeKeeping = dynamic(
+  () => import('../../ui/components/SafeKeeping'),
+  {
+    ssr: false,
+  }
+)
+const DynamicWithdrawal = dynamic(
+  () => import('../../ui/components/Withdrawal'),
+  {
+    ssr: false,
+  }
+)
 export default function Admin() {
   return (
     <Box>
-      <Mint />
+      <DynamicMint />
       <Divider />
-      <Invest />
+      <DynamicInvest />
       <Divider />
-      <SafeKeeping />
+      <DynamicSafeKeeping />
       <Divider />
-      <Withdrawal />
+      <DynamicWithdrawal />
     </Box>
   )
 }
